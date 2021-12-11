4109460c94a7ac2c817217868f3f6e39f2c051f9
sjchanson <13560469332@163.com>
2021-12-11 14:38:40 +0800

    sjc update.

--- legalization/CellMovement.h
+++ legalization/CellMovement.h
@@ -1,9 +1,35 @@
 /*
  * @Author: sjchanson
  * @Date: 2021-12-10 21:01:02
- * @LastEditTime: 2021-12-10 21:01:02
+ * @LastEditTime: 2021-12-11 14:34:29
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置:
  * https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /EDA_Q4/legalization/CellMovement.h
  */
+
+#ifndef CELL_MOVEMENT_CHECK
+#define CELL_MOVEMENT_CHECK
+
+#include <map>
+#include <vector>
+
+#include "LegalizationCheck.h"
+
+namespace EDA_CHALLENGE_Q4 {
+
+class CellMovement {
+ public:
+  CellMovement() = delete;
+  CellMovement(VCG* vcg);
+  ~CellMovement() = default;
+
+ private:
+  VCG* _vcg;
+
+  LegalizationCheck* _checker;
+};
+
+}  // namespace EDA_CHALLENGE_Q4
+
+#endif
--- legalization/LegalizationCheck.h
+++ legalization/LegalizationCheck.h
@@ -1,7 +1,7 @@
 /*
  * @Author: sjchanson
  * @Date: 2021-12-10 21:00:35
- * @LastEditTime: 2021-12-11 12:59:07
+ * @LastEditTime: 2021-12-11 14:36:01
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置:
  * https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -14,6 +14,7 @@
 #include <map>
 #include <vector>
 
+#include "../includes/Point.hpp"
 #include "../includes/VCG.hpp"
 
 namespace EDA_CHALLENGE_Q4 {
@@ -23,8 +24,21 @@ class LegalizationCheck {
   LegalizationCheck(VCG* vcg);
   ~LegalizationCheck() = default;
 
+  bool isLeftViolated(VCGNode* node);
+  bool isDownViolated(VCGNode* node);
+
+  std::vector<VCGNode*> obtainXLeftNodes(Point lower_y, Point upper_y);
+  std::vector<VCGNode*> obtainYDownNodes(Point lower_x, Point upper_x);
+
+  void updateVertexXYCoordRange(VCGNode* node);
+
  private:
   VCG* _vcg;
+
+  // record x y range.
+  std::map<VCGNode*, Point> _vertex_to_x_range;
+  std::map<VCGNode*, Point> _vertex_to_y_range;
+
 };  // namespace LegalizationCheck
 
 inline LegalizationCheck::LegalizationCheck(VCG* vcg) : _vcg(vcg) {}
